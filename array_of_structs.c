e26cbf6fbb2d52745ca639bc6bda4e8d365183f4
Ivo Georgiev <ivogeorg@gmail.com>
2016-02-24 18:25:44 -0700

    Create array_of_structs.c

---
+++ array_of_structs.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _some_struct {
+    int a; // 4 -> 8 for memory alignment
+    int *b; // 8
+    char *str; // 8
+    struct _some_struct *partner; // 8
+} some_struct_t, *some_struct_pt; // total size of struct = 32
+
+
+int main() {
+    printf("Hello, World!\n");
+    printf("sizeof(some_struct_t) = %lu!\n", (unsigned long) sizeof(some_struct_t));
+    const unsigned ARRAY_SIZE = 100;
+
+    some_struct_t static_ss;
+    static_ss.a = 10;
+    static_ss.b = &static_ss.a; // ??
+//    static_ss.str = (char *) calloc(1, sizeof(char));
+    static_ss.str = "array\n";
+    static_ss.partner = NULL;
+
+    some_struct_pt array_of_ss = (some_struct_pt) calloc(ARRAY_SIZE, sizeof(some_struct_t));
+
+    for (unsigned u=0; u<ARRAY_SIZE; u++) {
+        array_of_ss[u] = static_ss;
+    }
+
+    for (unsigned u=0; u<ARRAY_SIZE; u++) {
+        printf("%d-%d-%s\n", array_of_ss[u].a, *array_of_ss[u].b, array_of_ss[u].str);
+    }
+
+    free(array_of_ss);
+
+    return 0;
+}
